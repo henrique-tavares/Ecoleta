21da897e2f4ce84a7d90b7314422d88fb14775e2
henrique-tavares <ricktavares76@gmail.com>
2020-06-03 11:03:33 -0300

    back-end v1.0

---
+++ server/src/routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+// index, show, create, update, delete
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+routes.post('/points', pointsController.create);
+routes.put('/points/:id', pointsController.update);
+routes.delete('/points/:id', pointsController.delete);
+
+export default routes;
---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
+
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Alterar uma informação no back-end
+// DELETE: Remover uma informação no back-end
+
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação e etc
+// Request Body: Parâmetros para criação/atualização de informações
+
+app.listen(3333);
